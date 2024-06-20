989f0007e97c6bf74e3971b6000f0d11f932ad6e
thersihag <rsihagds@gmail.com>
2024-06-20 14:05:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-from flask import Flask, request, render_template
+from flask import Flask,request,render_template
 import numpy as np
 import pandas
 import sklearn
 import pickle
 
 # importing model
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('model.pkl','rb'))
+sc = pickle.load(open('standscaler.pkl','rb'))
+ms = pickle.load(open('minmaxscaler.pkl','rb'))
 
 # creating flask app
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
-@app.route("/predict", methods=['POST'])
+@app.route("/predict",methods=['POST'])
 def predict():
     N = request.form['Nitrogen']
     P = request.form['Phosporus']
@@ -28,7 +28,10 @@ def predict():
 
     feature_list = [N, P, K, temp, humidity, ph, rainfall]
     single_pred = np.array(feature_list).reshape(1, -1)
-    prediction = model.predict(single_pred)
+
+    scaled_features = ms.transform(single_pred)
+    final_features = sc.transform(scaled_features)
+    prediction = model.predict(final_features)
 
     crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
                  8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
@@ -40,7 +43,9 @@ def predict():
         result = "{} is the best crop to be cultivated right there".format(crop)
     else:
         result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
-    return render_template('index.html', result=result)
+    return render_template('index.html',result = result)
+
+
 
 
 # python main
